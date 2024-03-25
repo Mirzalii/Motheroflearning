c275776426f7bcca6676f8c9e8b52e56372042b3
Mirzalii <162805008+Mirzalii@users.noreply.github.com>
2024-03-25 19:49:14 +0500

    Update chapter.php
    Обновление кода из-за переноса файлов login.php, register.php в папку logic

--- cite/chapter.php
+++ cite/chapter.php
@@ -90,7 +90,7 @@ if ($loggedIn) {
   <div class="modal-content">
     <h2>Вход</h2>
     <!-- Форма входа -->
-    <form id="loginForm" method="post" action="login.php">
+    <form id="loginForm" method="post" action="logic/login.php">
       <p><label for="loginUsername">Имя пользователя:</label></p>
       <input type="text" id="loginUsername" name="username" required>
       <p><label for="loginPassword">Пароль:</label></p>
@@ -106,7 +106,7 @@ if ($loggedIn) {
   <div class="modal-content">
     <h2>Регистрация</h2>
     <!-- Форма регистрации -->
-    <form id="registerForm" method="post" action="register.php">
+    <form id="registerForm" method="post" action="logic/register.php">
     <label for="username"><p>Имя пользователя:</p> </label>
       <input type="text" id="username" name="username" required>
      <p><label for="password">Пароль:</label></p> 
